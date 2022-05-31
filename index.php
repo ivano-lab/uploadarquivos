da31d85d5a26d71fb8726406c54f4eb6263bea0d
Ívano Fontes <ivanofnts@gmail.com>
2022-05-31 16:59:36 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>uploadarquivos</title>
+</head>
+<body>
+    <h1>UPLOAD DE ARQUIVOS</h1>
+    <ul>
+        <li><a href="imagem.php">imagens</a></li>
+        <li><a href="documento.php">documentos</a></li>
+        <li><a href="audio.php">&aacute;udio</a></li>
+        <li><a href="video.php">v&iacute;deo</a></li>
+    </ul>
+</body>
+</html>
